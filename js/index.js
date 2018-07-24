892045dc6cde7fbbcb2f5dba184d705ebabb56d3
David Quilici <davidmquilici@yahoo.com>
2018-07-23 21:47:12 -0700

    First page built. A Tribute page.

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$("#Main").addClass("container-fluid well img-responsive");
+$("#Ignatius").addClass("containter-fluid img-responsive")
+
+$("button").addClass("btn btn-info");
+$(".quotes").addClass("well text-centered");
+$(".quotes").css("background-color", "silver");
